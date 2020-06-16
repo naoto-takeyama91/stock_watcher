3e4b9809f5198734b6ee6a228199885810435f5f
naoto-takeyama91 <naoto.takeyama0901@gmail.com>
2020-06-16 16:31:31 +0900

    update migration

--- db/migrate/20200507071008_create_items.rb
+++ db/migrate/20200507071008_create_items.rb
@@ -1,16 +1,13 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
-    create_table :items do |t|
+    create_table :items, id: :integer do |t|
       t.string :name
-      t.bigint :user_id, null: false
-      t.bigint :inventory_id, null: false
+      t.references :user
+      t.references :inventory
       t.string :image
       t.string :url
       t.integer :term, null: false
       t.timestamps
     end
-
-    add_foreign_key :items, :users
-    add_foreign_key :items, :inventories
   end
 end
--- db/migrate/20200507080127_create_purchase_histories.rb
+++ db/migrate/20200507080127_create_purchase_histories.rb
@@ -1,13 +1,10 @@
 class CreatePurchaseHistories < ActiveRecord::Migration[5.2]
   def change
-    create_table :purchase_histories do |t|
-      t.bigint :user_id, null: false
-      t.bigint :item_id, null: false
+    create_table :purchase_histories, id: :integer do |t|
+      t.references :user
+      t.references :item
       t.integer :amount, null: false
       t.timestamps
     end
-
-    add_foreign_key :purchase_histories, :users
-    add_foreign_key :purchase_histories, :items
   end
 end
